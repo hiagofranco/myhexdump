d8a16f92c654dc5cf4bdbf9c6d0f2d6d4905863b
Hiago De Franco <hfranco@baylibre.com>
2026-07-06 20:08:43 -0300

    myhexdump: improve code readability

--- myhexdump.c
+++ myhexdump.c
@@ -1,19 +1,43 @@
 #include <stdio.h>
 #include <ctype.h>
 
-#define BYTES_PER_LINE 0x10
-#define GROUP_LINE 0x8
+#define BYTES_PER_LINE 16
 
+/*
+ * Replace any non-printable char with '.'. Hexdump works like
+ * that and this helps the last printed string to no be meesed
+ * with '\n', '\t' and etc.
+ */
+void print_line_readable_text(unsigned char *buf, int cc)
+{
+	int pad = 3 * (BYTES_PER_LINE - cc) + (cc < BYTES_PER_LINE / 2 ? 1 : 0);
+
+	printf("%*s |", pad, "");
+	for (int i = 0; i < cc; i++)
+		printf("%c", isprint((unsigned char) buf[i]) ? buf[i] : '.');
+	printf("|\n");
+}
+
+void print_line_bytes(unsigned char *buf, int cc)
+{
+	for (int i = 0; i < cc; i++) {
+		printf("%02x ", buf[i]);
+		if (i == BYTES_PER_LINE / 2 - 1)
+			printf(" ");
+	}
+}
+
+void print_line_hex(int lc, int cc)
+{
+	printf("%08x ", lc * BYTES_PER_LINE + cc);
+}
 /*
  * Hexdump clone: This is for learning purposes.
  * Inputs a string, and the output will be equal to 'hexdump -C'.
  */
 int main(void)
 {
-	/*
-	 * String to be printed at the end of the line. I has exactly the
-	 * number of bytes printed per line plus the nul byte.
-	 */
+	/* +1 -> nul byte */
 	unsigned char s[BYTES_PER_LINE + 1];
 	/*
 	 * c: char read. Needs to be int because of EOF (0xff)
@@ -22,66 +46,26 @@ int main(void)
 	 */
 	int c, cc = 0, lc = 0;
 	while ((c = getchar()) != EOF) {
-		/*
-		 * Start of line: '00000000', '00000010' and so on...
-		 */
-		if (cc == 0)
-			printf("%08x  ", lc * BYTES_PER_LINE);
-
-		printf("%02x ", c);
-
-		/*
-		 * Replace any non-printable char with '.'. Hexdump works like
-		 * that and this helps the last printed string to no be meesed
-		 * with '\n', '\t' and etc.
-		 */
-		s[cc] = isprint((unsigned char) c) ? c : '.';
-		cc += 1;
-
-		/*
-		 * We want to divide the printed string with 16 bytes length
-		 * with two groups os 8 bytes.
-		 */
-		if (cc == GROUP_LINE)
-			printf(" ");
-		/*
-		 * If we reach the end of the 16 bytes, we need to go to the
-		 * next line. Therefore, we print the string with ASCII chars
-		 * and go to the next one.
-		 */
-		else if (cc == BYTES_PER_LINE) {
-			s[BYTES_PER_LINE] = '\0';
-			printf(" |%s|\n", s);
-			cc = 0;
-			lc += 1;
+		if (cc < BYTES_PER_LINE) {
+			s[cc] = (unsigned char) c;
+			cc += 1;
+			continue;
 		}
+		s[BYTES_PER_LINE] = '\0';
+		print_line_hex(lc, 0);
+		print_line_bytes(s, cc);
+		print_line_readable_text(s, cc);
+		cc = 0;
+		lc += 1;
 	}
-	/*
-	 * After reading all the bytes, if there are any bytes that did not
-	 * fill up until the last 16 bytes, we need to print them with some
-	 * padding to do not mess with the output.
-	 *
-	 * Example:
-	 * 00000010  67 6f 20 44 65 20 46 72  61 6e 63 6f 2c 20 48 65  |go De Franco, He|
-	 * 00000020  6c 6c 6f 20 3a 29 0a                              |llo :).|
-	 * 00000027
-	 */
 	if (cc > 0) {
-		/*
-		 * Padding: each hex by takes 3 spaces (2 numbers and a space,
-		 * like '65 '). At the end, we add 1 since we do an extra
-		 * printf(" ") with GROUP_LINE, only if it wasn't printed yet.
-		 */
-		int pad = 3 * (BYTES_PER_LINE - cc) + (cc < GROUP_LINE ? 1 : 0);
-		int pad2 = BYTES_PER_LINE - cc;
-		/*
-		 * Need to finish the string with the remaining chars because
-		 * it can print garbage from the string arrary
-		 */
 		s[cc] = '\0';
-		printf("%*s |%s%*s|\n", pad, "", s, pad2, "");
+		print_line_hex(lc, cc);
+		print_line_bytes(s, cc);
+		print_line_readable_text(s, cc);
 	}
-	printf("%08x\n", lc * BYTES_PER_LINE + cc);
+	print_line_hex(lc, 0);
+	printf("\n");
 
 	return 0;
 }
